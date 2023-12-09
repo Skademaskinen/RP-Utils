f657965e18cd13a5205baeeb0cee196ad1d891a4
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-12-09 08:21:00 +0100

    pdf

--- app/src/main/java/wordcount/actions/Makepdf.java
+++ app/src/main/java/wordcount/actions/Makepdf.java
@@ -5,11 +5,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -33,7 +37,7 @@ public class Makepdf implements Action{
             .addOption(OptionType.CHANNEL, "target", "Specify a category or channel to generate from", true);
     }
 
-    @Override
+    @Override @SuppressWarnings("deprecation")
     public CommandResponse execute() {
         // prepare
         ((SlashCommandInteractionEvent)event).deferReply(true).queue();
@@ -44,37 +48,47 @@ public class Makepdf implements Action{
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        List<TextChannel> channels;
-        if(((SlashCommandInteractionEvent)event).getOptions().get(0).getAsChannel().getType().equals(ChannelType.CATEGORY)){
-            channels = ((SlashCommandInteractionEvent)event).getOptions().get(0).getAsChannel().asCategory().getChannels().stream().map(channel -> (TextChannel)channel).toList();
+        List<MessageChannel> channels;
+        if(((SlashCommandInteractionEvent)event).getMessageChannel().getType().equals(ChannelType.PRIVATE)){
+            channels = new ArrayList<>(){{add(((SlashCommandInteractionEvent)event).getMessageChannel());}};
+        }
+        else if(((SlashCommandInteractionEvent)event).getOptions().get(0).getAsChannel().getType().equals(ChannelType.CATEGORY)){
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/tmp/wordcount-document/index.tex")))){
+                writer.append(String.format("\\chapter*{%s}", ((SlashCommandInteractionEvent)event).getGuild().getName()));
+            }
+            catch(IOException e){}
+            channels = ((SlashCommandInteractionEvent)event).getOptions().get(0).getAsChannel().asCategory().getChannels().stream().filter(channel -> channel.getType().equals(ChannelType.TEXT)).map(channel -> (MessageChannel)channel).toList();
         }
         else{
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/tmp/wordcount-document/index.tex")))){
+                writer.append(String.format("\\chapter*{%s}", ((SlashCommandInteractionEvent)event).getGuild().getName()));
+            }
+            catch(IOException e){}
             channels = new ArrayList<>(){{add(((SlashCommandInteractionEvent)event).getOptions().get(0).getAsChannel().asTextChannel());}};
         }
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/tmp/wordcount-document/index.tex")))){
-            writer.append(String.format("\\chapter{%s}", ((SlashCommandInteractionEvent)event).getGuild().getName()));
-        }
-        catch(IOException e){}
-        for(TextChannel channel : channels){
+        for(MessageChannel channel : channels){
             System.out.println("generating for channel: "+ channel.getName());
             try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/tmp/wordcount-document/index.tex"), true))){
                 writer.append(String.format("\\input{/tmp/wordcount-document/%s.tex}\n", channel.getName()));
             }
-            catch(IOException e){}
+            catch(IOException e){e.printStackTrace();}
             try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File(String.format("/tmp/wordcount-document/%s.tex", channel.getName()))))){
-                writer.append(String.format("\\section{%s}\n", channel.getName()));
+                writer.append(String.format("\\section{%s}\n", channel.getName().replace("_", "\\_")));
                 List<Message> messages = new ArrayList<>();
                 channel.getIterableHistory().forEach(message -> {
                     messages.add(message);
+                    System.out.println("Processing new message: "+message.getId());
                 });
                 for(Message message : messages.reversed()){
                     String paragraph, content;
                     if(message.getContentDisplay().split(" ").length > 1){
-                        paragraph = message.getContentDisplay().split(" ",2)[0];
-                        content = message.getContentDisplay().split(" ", 2)[1];
+                        String allowedCharsRegex = "[\\w\\s\\?.,/æøåÆØÅ\"#()<>:;']";
+                        String messageContent = message.getContentDisplay().chars().mapToObj(c -> String.valueOf((char)c)).filter(c -> c.matches(allowedCharsRegex)).map(c -> c.matches("[_%#]") ? "\\"+c : c).collect(Collectors.joining());
+                        paragraph = messageContent.replace("\n", " ").split(" ",2)[0];
+                        content = messageContent.substring(paragraph.length());
                     }
                     else{
-                        paragraph = message.getAuthor().getName() + message.getId();
+                        paragraph = message.getAuthor().getName().replace("_", "\\_") + message.getId();
                         content = "error";
                     }
                     try {
@@ -82,25 +96,34 @@ public class Makepdf implements Action{
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    // maybe do attachments as well?
+                    for(Attachment attachment : message.getAttachments().stream().filter(attachment -> attachment.isImage()).collect(Collectors.toList())){
+                        if(attachment.getFileName().length() > 50 || attachment.getFileExtension().equals("gif")) continue;
+                        attachment.downloadToFile("/tmp/wordcount-document/"+attachment.getFileName()).complete(null);
+                        writer.append(String.format("\\begin{figure}[H]\n\t\\centering\n\t\\includegraphics[width=\\textwidth]{%s}\n \\end{figure}", "/tmp/wordcount-document/"+attachment.getFileName()));
+                    }
                 };
+                System.out.println("finished writing!");
             }
-            catch(IOException e){}
+            catch(IOException e){e.printStackTrace();}
         }
         return new CommandResponse() {
             @Override
             public void execute() {
                 // generate
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 String[] command = {"bash", "../document-template/generate-pdf.sh"};
                 try {
                     Process process = runtime.exec(command);
-                    if(process.waitFor() != 0){
-                        System.err.println("Fuck");
-                        for(String line : process.errorReader().lines().toList()){
-                            System.err.println(line);
-                        }
-                        return;
-                    } 
-                } catch (IOException | InterruptedException e) {
+
+                    for(String line : process.errorReader().lines().toList()) System.err.println(line);
+                    for(String line : process.inputReader().lines().toList()) System.err.println(line);
+
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 ((SlashCommandInteractionEvent)event).getHook().editOriginal("Finished execution").setAttachments(AttachedFile.fromData(new File(String.format("../version-log/document%d.pdf", new File("../version-log").listFiles().length-1)))).queue();
--- document-template/generate-pdf.sh
+++ document-template/generate-pdf.sh
@@ -1,4 +1,4 @@
-pdflatex -interaction=nonstopmode ../document-template/master.tex
+xelatex -interaction=nonstopmode ../document-template/master.tex
 rm master.aux
 rm master.log
 mv master.pdf ../version-log/document$(python -c 'from sys import argv; print(len(argv[1].split("\n")) if argv[1] != "" else 0)' "$(ls ../version-log)").pdf
